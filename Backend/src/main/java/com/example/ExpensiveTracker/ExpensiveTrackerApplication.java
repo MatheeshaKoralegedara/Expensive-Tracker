31bcce2a40c92a251e749e0729fedf3f257bff46
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-03-31 09:29:27 +0530

    Initialize Springboot and create project structure

---
+++ Backend/src/main/java/com/example/ExpensiveTracker/ExpensiveTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExpensiveTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensiveTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensiveTrackerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/ExpensiveTracker/ExpensiveTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExpensiveTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiveTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
